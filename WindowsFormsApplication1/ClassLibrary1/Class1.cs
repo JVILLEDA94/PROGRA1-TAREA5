67d631fb617dae2b2dbccd37fdb092a7a87a7832
JVILLEDA94 <47669945+JVILLEDA94@users.noreply.github.com>
2019-03-31 12:40:41 -0600

    Add files via upload

---
+++ WindowsFormsApplication1/ClassLibrary1/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private double d1, d2, resul;
+
+        public double Resul
+        {
+            get { return resul; }
+            set { resul = value; }
+        }
+
+        public double D2
+        {
+            get { return d2; }
+            set { d2 = value; }
+        }
+
+        public double D1
+        {
+            get { return d1; }
+            set { d1 = value; }
+        }
+
+        public Class1()
+        {
+            this.d1 = 0;
+            this.d2 = 0;
+        }
+
+        public double CalcularSuma()
+        {
+            resul = d1 + d2;
+            return resul;
+        }
+
+        public double CalcularResta()
+        {
+            resul = d1 - d2;
+            return resul;
+        }
+
+        public double CalcularMulti()
+        {
+            resul = d1 * d2;
+            return resul;
+        }
+
+        public double CalcularDiv()
+        {
+            resul = d1 / d2;
+            return resul;
+        }
+        
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Class1 Operacion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Operacion = new Class1();
+            string resultado;
+
+            if ((string.IsNullOrEmpty(textBox1.Text)) || (string.IsNullOrEmpty(textBox2.Text)))
+            {
+                MessageBox.Show("Ingrese valores", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                Operacion.D1 = double.Parse(textBox1.Text);
+                Operacion.D2 = double.Parse(textBox2.Text);
+
+            }
+            
+            if (this.radioButton1.Checked == true)
+            {
+                resultado = Operacion.CalcularSuma().ToString();
+                MessageBox.Show("El resultado es: "+resultado);
+            }
+            if (this.radioButton2.Checked == true)
+            {
+                resultado = Operacion.CalcularResta().ToString();
+                MessageBox.Show("El resultado es: " + resultado);
+            }
+            if (this.radioButton3.Checked == true)
+            {
+                resultado = Operacion.CalcularMulti().ToString();
+                MessageBox.Show("El resultado es: " + resultado);
+            }
+            if (this.radioButton4.Checked == true)
+            {
+                if (Operacion.D2 == 0)
+                {
+                    MessageBox.Show("No puedes dividir entre 0", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    resultado = Operacion.CalcularDiv().ToString();
+                    MessageBox.Show("El resultado es: " + resultado);
+                }
+            }
+            if ((this.radioButton1.Checked == false) && (this.radioButton2.Checked == false) && (this.radioButton3.Checked == false) && (this.radioButton4.Checked == false))
+            {
+                MessageBox.Show("Ninguna opcion seleccionado", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);  
+            }
+
+
+        }
+    }
+}
